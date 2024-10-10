862bb09da21dbba3727aa778cbca583e4e2f3bdd
Guilherme Casagrande <thegui4000@gmail.com>
2024-10-10 20:44:27 -0300

    feat: create deafault component & navbar component

---
+++ resources/views/components/background.blade.php
@@ -0,0 +1,29 @@
+@props([
+    'title',
+    'active',
+    'content',
+
+])
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title> {{ $title }} </title>
+
+    <style>
+        .welcome{
+            min-height: calc(100vh - 56px);
+        }
+    </style>
+</head>
+<body class="d-flex flex-column boder vh-100">
+    <x-menu.navbar active="{{ $active }}" />
+    
+    <div class="container {{ $active == 'welcome' ? 'd-flex justify-content-center welcome' : '' }}">{{ $content }}</div>
+    
+    
+</body>
+</html>
---
+++ resources/views/components/menu/navbar.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'active',     
+])
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+    <div class="container">
+        <a class="navbar-brand d-flex flex-nowrap" href="#">
+            <img src="./SignoTech.webp"  height="30" alt="">
+            <!-- <span class="mx-2">PS-GuilhermeCasagrande</span> -->
+        </a>
+        <span>
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <a class="nav-link {{ $active == 'welcome' ? 'active' : ''}}" href="/">Home</a>
+                    <a class="nav-link {{ $active == 'view' ? 'active' : ''}}" href="/poll">Enquetes</a>
+                    <a class="nav-link {{ $active == 'create' ? 'active' : ''}}" href="/poll/create">Create</a>
+                </div>
+            </div>
+        </span>
+        
+    </div>
+</nav>
